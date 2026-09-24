422a1ad9b406ea524642bed7b0ba456cb93bae57
kinderDmitry <kinderMikhail0vich@gmail.com>
2026-09-24 11:15:22 +0300

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-where gradle >nul 2>nul
-if %ERRORLEVEL% EQU 0 ( gradle %* & exit /b %ERRORLEVEL% )
-echo Gradle 8.11.1 is required. Install Gradle or run this project in Android Studio.
-exit /b 1
